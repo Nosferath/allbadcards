0afda77ac4acf05e724c4ae18494ea26b1c6f6a9
Jake Lauer <jake.lauer@gmail.com>
2020-05-29 19:26:01 -0700

    - Linux/Mac compatibility

--- server/scripts/delete-output.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs-extra');
-
-const appDirectory = fs.realpathSync(process.cwd());
-const resolve = relativePath => path.resolve(appDirectory, relativePath);
-const outputDir = resolve(`output`);
-
-fs.rmdir(outputDir, {recursive: true});
