59a6a3de02dbd85e49613cd109513e40eb6e03cb
Jake Lauer <jake.lauer@gmail.com>
2020-06-15 08:55:07 -0700

    Forgot to update the JSON import schema (#364)

--- client/src/Areas/Game/Components/Gameplay/Support.tsx
+++ client/src/Areas/Game/Components/Gameplay/Support.tsx
@@ -2,12 +2,12 @@ import {ButtonGroup, colors, ListItem, ListItemText, Typography, useMediaQuery} 
 import React, {useEffect, useState} from "react";
 import Button from "@material-ui/core/Button";
 import {Twemoji} from "react-emoji-render";
-import {Platform} from "../../../../Global/Platform/platform";
+import {Platform} from "@Global/Platform/platform";
 import List from "@material-ui/core/List";
 import {FaDollarSign, FaPaypal, RiExternalLinkLine} from "react-icons/all";
 import {makeStyles} from "@material-ui/styles";
-import {EnvDataStore} from "../../../../Global/DataStore/EnvDataStore";
-import {useDataStore} from "../../../../Global/Utils/HookUtils";
+import {EnvDataStore} from "@Global/DataStore/EnvDataStore";
+import {useDataStore} from "@Global/Utils/HookUtils";
 
 const useStyles = makeStyles({
 	link: {
@@ -33,24 +33,16 @@ export const Support = () =>
 	}
 
 	const thankYous = [
-		<Button
-			variant={"contained"}
-			color={"secondary"}
-			style={{color: "white", textDecoration: "none", marginTop: "1rem", backgroundColor: "#058dc7"}}
-			startIcon={<Twemoji text={"☕"}/>}
-			onClick={() => Platform.trackEvent("support-link-click", "kofi")}
-			href="https://ko-fi.com/A76217J" target="_blank"
-		>
-			Buy me a coffee
-		</Button>,
-		<a href='https://ko-fi.com/A76217J' target='_blank' onClick={() => Platform.trackEvent("support-link-click", "kofi-coffee")}>
-			<img
-				height='36'
-				style={{border: 0, height: 36, marginTop: "1rem"}}
-				src='https://cdn.ko-fi.com/cdn/kofi2.png?v=2'
-				alt='Buy Me a Coffee at ko-fi.com'
-			/>
-		</a>,
+		<>
+			<div>
+				<a href={"http://patreon.com/allbadcards"} target={"_blank"} rel={"noreferrer nofollow"}>
+					<img src={"/become_a_patron_button.png"}/>
+				</a>
+			</div>
+			<div>
+				- OR -
+			</div>
+		</>,
 		<Button
 			variant={"contained"}
 			color={"secondary"}
@@ -81,14 +73,14 @@ export const Support = () =>
 					<ListItem>
 						<ListItemText style={{textAlign: "center"}} primary={<>
 							<ButtonGroup orientation={mobile ? "vertical" : "horizontal"}>
-								<Button style={{background: colors.blue.A100}} startIcon={<img width={18} src={"https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg"} />} endIcon={<RiExternalLinkLine/>} variant={"contained"} size={"large"} className={classes.link} href={"https://venmo.com/allbadcards"} target={"_blank"}>
+								<Button style={{background: colors.blue.A100}} startIcon={<img width={18} src={"https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg"}/>} endIcon={<RiExternalLinkLine/>} variant={"contained"} size={"large"} className={classes.link} href={"https://venmo.com/allbadcards"} target={"_blank"}>
 									venmo
 								</Button>
 								<Button style={{background: colors.orange.A100}} startIcon={<FaPaypal/>} endIcon={<RiExternalLinkLine/>} variant={"contained"} size={"large"} className={classes.link} href={"https://paypal.me/jakelauer"} target={"_blank"}>
 									paypal
 								</Button>
 								<Button  style={{background: colors.green.A100}} startIcon={<FaDollarSign />} endIcon={<RiExternalLinkLine/>} variant={"contained"} size={"large"} className={classes.link} href={"https://cash.app/$allbadcards"} target={"_blank"}>
-									$cash
+									cashapp
 								</Button>
 							</ButtonGroup>
 						</>}/>
--- client/src/Areas/Pack/Create/schema.ts
+++ client/src/Areas/Pack/Create/schema.ts
@@ -5,42 +5,17 @@ export const AbcPackSchema: JSONSchema4 = {
 	"$schema": "http://json-schema.org/draft-04/schema#",
 	"type": "object",
 	"properties": {
-		"black": {
-			"type": "array",
-			"items": {"$ref": "#/definitions/blackCard"}
-		},
-		"white": {
+		"packName": {"type": "string"},
+		"blackCards": {
 			"type": "array",
 			"items": {"type": "string"}
 		},
-		"pack": {"$ref": "#/definitions/packMeta"},
-		"quantity": {
-			"type": "object",
-			"properties": {
-				"black": {"type": "integer"},
-				"white": {"type": "integer"},
-				"total": {"type": "integer"},
-			}
+		"whiteCards": {
+			"type": "array",
+			"items": {"type": "string"}
 		}
 	},
-	"definitions": {
-		"blackCard": {
-			"type": "object",
-			"properties": {
-				"content": {"type": "string"},
-				"draw": {"type": "integer", "minimum": 0, "maximum": 2},
-				"pick": {"type": "integer", "minimum": 1, "maximum": 3},
-			},
-			"required": ["content"]
-		},
-		"packMeta": {
-			"type": "object",
-			"properties": {
-				"id": {"type": "string"},
-				"name": {"type": "string"},
-			}
-		}
-	}
+	"required": ["packName", "blackCards", "whiteCards"]
 };
 
 const validator = new Validator();
